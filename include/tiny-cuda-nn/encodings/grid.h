73ea46a5dcaa2dec68f49e5b920d46f6ec8f5f6a
seungjooshin <seungjooshin.research@gmail.com>
2023-03-15 20:36:13 +0900

    update binarization

--- include/tiny-cuda-nn/encodings/grid.h
+++ include/tiny-cuda-nn/encodings/grid.h
@@ -340,8 +340,8 @@ __global__ void kernel_grid(
 			TCNN_PRAGMA_UNROLL
 			for (uint32_t feature = 0; feature < N_FEATURES_PER_LEVEL; ++feature) {
 				float data = (float)((T*)&val)[feature];
-				if (data >= 0.f) data = 1.0f;
-				else data = -1.0f;
+				data = (data >= 0.f) ? 1.0f : -1.0f;
+				if (fabsf(data) < quantize_threshold) data = 0.f;
 				((T*)&result)[feature] += (T)(weight * data);
 			}
 		}
