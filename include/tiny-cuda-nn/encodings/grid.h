0cb73e8a7b1f3dc00bf9fd0380eae9c702cb59d7
seungjooshin <seungjooshin.research@gmail.com>
2023-03-26 17:15:51 +0900

    apply hard tanh at forward()

--- include/tiny-cuda-nn/encodings/grid.h
+++ include/tiny-cuda-nn/encodings/grid.h
@@ -338,7 +338,7 @@ __global__ void kernel_grid(
 			for (uint32_t feature = 0; feature < N_FEATURES_PER_LEVEL; ++feature) {
 				float data = (float)((T*)&val)[feature];
 				data = (data > 1.0f) ? 1.0f : data;
-				ata = (data < -1.0f) ? -1.0f : data;
+				data = (data < -1.0f) ? -1.0f : data;
 				// data = (data >= 0.f) ? 1.0f : -1.0f;
 				if (fabsf(data) < quantize_threshold) data = 0.f;
 				((T*)&result)[feature] += (T)(weight * data);
