e4109259f2d76185f2cb54d4de247fbf0265e812
seungjooshin <seungjooshin.research@gmail.com>
2023-04-10 17:22:35 +0900

    update

--- include/tiny-cuda-nn/encodings/grid.h
+++ include/tiny-cuda-nn/encodings/grid.h
@@ -354,7 +354,8 @@ __global__ void kernel_grid(
 					float data = (float)((T*)&val)[feature];
 					if (fabsf(data) < quantize_threshold) data = 0.f;
 					data = data > 0 ? 1.0f : -1.0f; // apply binary activation function
-					((T*)&result)[feature] += (T)(weight * data);
+					if (interpolation_type == InterpolationType::BinaryLinear) data *= weight
+					((T*)&result)[feature] += (T)(data);
 				}
 			}
 			else {
