28ba2447d1fb30e80dc87c876c5bddef1424ef1c
seungjooshin <seungjooshin.research@gmail.com>
2023-04-27 22:44:25 +0900

    add `BinaryNearest`

--- bindings/torch/tinycudann/modules.py
+++ bindings/torch/tinycudann/modules.py
@@ -366,7 +366,7 @@ class Encoding(Module):
 		self.binary = False
 		if 'interpolation' in encoding_config.keys():
 			interpolation = encoding_config['interpolation']
-			self.binary = True if (interpolation == 'BinaryLinear' or interpolation == 'BinaryLinearApprox') else False
+			self.binary = True if (interpolation == 'BinaryLinear' or interpolation == 'BinaryLinearApprox' or interpolation == 'BinaryNearest') else False
 
 	def _native_tcnn_module(self):
 		return _C.create_encoding(self.n_input_dims, self.encoding_config, self.precision)
